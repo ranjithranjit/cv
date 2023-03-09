0d26e215d52fdc5cf756f72ae3022ccf11004b9d
RANJITH <106427495+ranjithranjit@users.noreply.github.com>
2023-03-09 08:08:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ tl.to(".text",{y:"0%",duration: 2, stagger: .025, delay:2});
 tl.to(".page", {y:"-100%", duration: 1.2, delay: .5, delay:1});
 tl.to(".loader", {y:"-100%", duration: 1} ,"-=1"); 
 function myfunction(){
+  gsap.to(".alert", {opacity: 0, duration: 2})
 gsap.to(".box-1",{duration:1, y:"0%", ease:"bounce"});
 gsap.to(".box-2",{duration:1, y:"0%", ease:"bounce.out",delay:1})
 gsap.to(".box-3",{duration:1, y:"0%", ease:"bounce", delay: 1.5})
@@ -47,7 +48,7 @@ function textBack(){
 }
 
 function fun(){
-    gsap.to(".alert", {opacity: 0, duration: 2, display:none})
+    gsap.to(".alert", {opacity: 0, duration: 2})
 }
 
 
